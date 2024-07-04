137a80053180ebceedc92a82d56f0e78eb2878ff
rukhsarbreathingai <rukhsarrashidkhan@breathing.ai>
2024-07-03 22:59:35 -0400

    Initial Commit

---
+++ browse_recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  browse_recipes
+//
+//  Created by Rukhsar Khan on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ browse_recipes/browse_recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  browse_recipesApp.swift
+//  browse_recipes
+//
+//  Created by Rukhsar Khan on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct browse_recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
